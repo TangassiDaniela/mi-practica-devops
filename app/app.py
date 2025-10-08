c2b5369f9572c227e1d0f93191cc98f8257c7276
Daniela Tangassi <daniela.tangassi@gmail.com>
2025-10-07 18:32:03 -0600

    Parte1: app Flask y tests

---
+++ app/app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return ("<h1>Materia: DevOps</h1>"
+            "<p>Profesor: Nombre del Profesor</p>"
+            "<p>Alumno: Tu Nombre</p>")
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
